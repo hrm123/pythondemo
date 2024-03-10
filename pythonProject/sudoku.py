1ab422763f9e3536bf5866aad95f1ff63f54a307
h_r_m <pitechworks@gmail.com>
2024-03-10 09:46:28 -0700

    easy sudoku solver

--- pythonProject/sudoku.py
+++ pythonProject/sudoku.py
@@ -1,26 +1,28 @@
 from utils import grid_values
-from utils import display
+from utils import display, parse_grid
+from utils import test_sudoku_setup 
 
+# https://norvig.com/sudoku.html
 rows = 'ABCDEFGHI'
 cols = '123456789'
 
 def cross(a,b):
     return [s+t for s in a for t in b ]
 
-squares = cross(rows, cols)
-print('squares')
+cells = cross(rows, cols)
+print('cells')
 print('--------------------------')
-print( squares)
+print( cells)
 
-row_units = [cross(r, cols) for r in rows]
-print('row_units')
+row_cells = [cross(r, cols) for r in rows]
+print('row_cells')
 print('--------------------------')
-print( row_units)
+print( row_cells)
 
-col_units = [cross(rows, c) for c in cols]
-print('col_units')
+col_cells = [cross(rows, c) for c in cols]
+print('col_cells')
 print('--------------------------')
-print( col_units)
+print( col_cells)
 
 sq_units = [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123','456','789')]
 print('sq_units')
@@ -29,4 +31,35 @@ print( sq_units)
 
 print('grid_values')
 print('--------------------------')
-display(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..', squares), squares,rows,cols)
+display(grid_values('..3.2.6..9..3.5..1..18.64....81.29..7.......8..67.82....26.95..8..2.3..9..5.1.3..', cells), cells,rows,cols)
+
+unitlist = ([cross(rows, c) for c in cols] +
+            [cross(r, cols) for r in rows] +
+            [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')])
+
+print('col_cells')
+print('--------------------------')
+print( col_cells)
+
+units = dict((s, [u for u in unitlist if s in u]) 
+             for s in cells)
+print('unitlist')
+print('--------------------------')
+print( unitlist)
+
+print('units')
+print('--------------------------')
+print(units)
+
+peers = dict((s, set(sum(units[s],[]))-set([s]))
+             for s in cells)
+print('peers')
+print('--------------------------')
+print( peers)
+
+test_sudoku_setup(cells, unitlist, units, peers)
+
+
+grid1 = '003020600900305001001806400008102900700000008006708200002609500800203009005010300'
+
+display(parse_grid(grid1,cells,units, peers),cells,rows,cols)
--- pythonProject/utils.py
+++ pythonProject/utils.py
@@ -91,3 +91,63 @@ def display(values, squares, rows, cols):
 
 if __name__ == '__main__':
     main()
+
+
+def test_sudoku_setup(squares, unitlist, units, peers):
+    "A set of unit tests."
+    assert len(squares) == 81
+    assert len(unitlist) == 27
+    assert all(len(units[s]) == 3 for s in squares)
+    assert all(len(peers[s]) == 20 for s in squares)
+    assert units['C2'] == [['A2', 'B2', 'C2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2'],
+                           ['C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9'],
+                           ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'C1', 'C2', 'C3']]
+    assert peers['C2'] == set(['A2', 'B2', 'D2', 'E2', 'F2', 'G2', 'H2', 'I2',
+                               'C1', 'C3', 'C4', 'C5', 'C6', 'C7', 'C8', 'C9',
+                               'A1', 'A3', 'B1', 'B3'])
+    print('All tests pass.')
+
+    
+def parse_grid(grid, cells, units,peers):
+    """Convert grid to a dict of possible values, {square: digits}, or
+    return False if a contradiction is detected."""
+    digits   = '123456789'
+    ## To start, every square can be any digit; then assign values from the grid.
+    values = dict((s, digits) for s in cells)
+    for s,d in grid_values(grid, cells).items():
+        if d in digits and not assign(values, s, d, units,peers):
+            return False ## (Fail if we can't assign d to square s.)
+    return values
+
+def assign(values, s, d, units, peers):
+    """Eliminate all the other values (except d) from values[s] and propagate.
+    Return values, except return False if a contradiction is detected."""
+    other_values = values[s].replace(d, '')
+    if all(eliminate(values, s, d2, units, peers) for d2 in other_values):
+        return values
+    else:
+        return False
+    
+def eliminate(values, s, d, units,peers):
+    """Eliminate d from values[s]; propagate when values or places <= 2.
+    Return values, except return False if a contradiction is detected."""
+    if d not in values[s]:
+        return values ## Already eliminated
+    values[s] = values[s].replace(d,'')
+    ## (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
+    if len(values[s]) == 0:
+        return False ## Contradiction: removed last value
+    elif len(values[s]) == 1:
+        d2 = values[s]
+        if not all(eliminate(values, s2, d2, units, peers) for s2 in peers[s]):
+            return False
+    ## (2) If a unit u is reduced to only one place for a value d, then put it there.
+    for u in units[s]:
+        dplaces = [s for s in u if d in values[s]]
+        if len(dplaces) == 0:
+            return False ## Contradiction: no place for this value
+        elif len(dplaces) == 1:
+            # d can only be in one place in unit; assign it there
+                if not assign(values, dplaces[0], d, units, peers):
+                    return False
+    return values
